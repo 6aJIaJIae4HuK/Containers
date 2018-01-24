68e97070d030616c4de8c9a08b89246bbaeaeac4
Timur Muratshin <timuratshin@yandex.ru>
2018-01-24 23:27:02 +0300

    Added empty boost tests

---
+++ tests/list_iterator_tests.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_NO_LIB
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(listIterator)
+
+BOOST_AUTO_TEST_CASE(empty_test)
+{
+	BOOST_CHECK(true == true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
