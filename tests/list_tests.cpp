1d1f2f24f4e572e6b09ffb9cafeb0d860ea4e516
Timur Muratshin <timuratshin@yandex.ru>
2018-02-08 17:17:20 +0300

    Changed gradle to CMake

--- tests/list_tests.cpp
+++ tests/list_tests.cpp
@@ -1,4 +1,4 @@
-//#define BOOST_TEST_NO_LIB
+#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
 #include "../include/list.h"
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,2 +1,12 @@
-#define BOOST_TEST_MAIN
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+bool init_unit_test()
+{
+  return true;
+}
+
+int main(int argc, char* argv[])
+{
+  return boost::unit_test::unit_test_main( &init_unit_test, argc, argv );
+}
